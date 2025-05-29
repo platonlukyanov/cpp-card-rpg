ac481609f10c742290171ef8249fa70323e6bc7a
Platon Lukyanov <platonioluka@gmail.com>
2025-05-29 23:31:37 +0300

    Added testing report

--- src/card_cli_utils.cpp
+++ src/card_cli_utils.cpp
@@ -63,7 +63,8 @@ void displayCard(std::shared_ptr<Card> card) {
             printStat("Trust", leverageCard->getTrustDamage(), Color::BLUE);
             break;
         }
-        default:
+        default
+        :
             std::cout << std::endl;
     }
 }
